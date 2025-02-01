afdbc497ea20d88d6075b500605a02850da1801b
musoftware <mkazi581@gmail.com>
2025-02-01 21:56:44 +0300

    some fixes and upgrades

--- lib/ciphers.dart
+++ lib/ciphers.dart
@@ -2,22 +2,14 @@ import 'package:flutter/material.dart';
 
 /// Flutter code sample for [Card].
 
-void main() => runApp(const CardExampleApp());
-
 class CardExampleApp extends StatelessWidget {
   const CardExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blueGrey,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Ciphers')),
-        body: const CardExample(),
-      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Ciphers')),
+      body: const CardExample(),
     );
   }
 }
@@ -47,7 +39,9 @@ class CardExample extends StatelessWidget {
                     children: <Widget>[
                       TextButton(
                         child: const Text('LEARN MORE'),
-                        onPressed: () {/* ... */},
+                        onPressed: () {
+                          /* ... */
+                        },
                       ),
                       const SizedBox(width: 6),
                     ],
@@ -63,14 +57,17 @@ class CardExample extends StatelessWidget {
                   const ListTile(
                     leading: Icon(Icons.album),
                     title: Text('Vernam cipher'),
-                    subtitle: Text('It is a method of encrypting alphabetic text by using a simple form of polyalphabetic substitution.'),
+                    subtitle: Text(
+                        'It is a method of encrypting alphabetic text by using a simple form of polyalphabetic substitution.'),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
                       TextButton(
                         child: const Text('LEARN MORE'),
-                        onPressed: () {/* ... */},
+                        onPressed: () {
+                          /* ... */
+                        },
                       ),
                       const SizedBox(width: 6),
                     ],
@@ -86,15 +83,17 @@ class CardExample extends StatelessWidget {
                   const ListTile(
                     leading: Icon(Icons.album),
                     title: Text('The Hill cipher'),
-                    subtitle: Text('Advanced Encryption Standard is a specification for the encryption of electronic data established by the U.S. National Institute of Standards and Technology.'),
-                    
+                    subtitle: Text(
+                        'Advanced Encryption Standard is a specification for the encryption of electronic data established by the U.S. National Institute of Standards and Technology.'),
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: <Widget>[
                       TextButton(
                         child: const Text('LEARN MORE'),
-                        onPressed: () {/* ... */},
+                        onPressed: () {
+                          /* ... */
+                        },
                       ),
                       const SizedBox(width: 6),
                     ],
@@ -112,7 +111,9 @@ class CardExample extends StatelessWidget {
                     children: <Widget>[
                       TextButton(
                         child: const Text('LEARN MORE'),
-                        onPressed: () {/* ... */},
+                        onPressed: () {
+                          /* ... */
+                        },
                       ),
                       const SizedBox(width: 6),
                     ],
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,60 +1,52 @@
 import 'package:flutter/material.dart';
 
-
 class ScreenLogin extends StatelessWidget {
   const ScreenLogin({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blueGrey,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cryptography App'),
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Cryptography App'),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Align(
-                alignment: Alignment(0.0, -0.9),
-                child: Text(
-                  'Log in',
-                  textDirection: TextDirection.ltr,
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontFamily: 'Roboto',
-                  ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Align(
+              alignment: Alignment(0.0, -0.9),
+              child: Text(
+                'Log in',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'Roboto',
                 ),
               ),
-              SizedBox(height: 20),
-              TextField(
-
-                decoration: InputDecoration(
-                  labelText: 'Gmail',
-                ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Gmail',
               ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-
-                  labelText: 'Password',
-                ),
-                obscureText: true,
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
               ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-
-                },
-                child: Text('Register'),
-              ),
-            ],
-          ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: Text('Register'),
+            ),
+          ],
         ),
       ),
     );
