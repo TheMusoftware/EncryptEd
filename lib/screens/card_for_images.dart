c984b7340904fc1331ee5418e15136e45c303dde
Liubou <132359274+LiubovMtsvch@users.noreply.github.com>
2025-02-05 22:35:22 +0300

    update

--- lib/screens/card_for_images.dart
+++ lib/screens/card_for_images.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(CardMap());
 }
+
 class CardMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -13,36 +14,73 @@ class CardMap extends StatelessWidget {
       ),
       home: Scaffold(
         body: Center(
-          child: Container(
-            width: 200,
-            height: 265,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.purple.withOpacity(0.6),
-                  spreadRadius: 3, 
-                  blurRadius: 10,
-                  offset: Offset(0, 3),
-                  
-                  
-                )
-              ]
-            ),
-            child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-              child:Column(
-                children: [
-                  Container(
-                    child: Image.asset("assets/logo.png"),
-                    
-                  )
-
-                ],
-              ) ,
-            ),
-
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 265,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.purple.withOpacity(0.6),
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Image.asset("assets/logo.png"),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 50), // space between the cards
+              Container(
+                width: 300,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.purple.withOpacity(0.6),
+                      spreadRadius: 3,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Image.asset("assets/logo.png"),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: Text(
+                          "Horizontal Card",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
