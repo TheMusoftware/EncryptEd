e32c262ae6c17513e91a40a1f1b3294c350a7326
Mustafa Kazı <mkazi581@gmail.com>
2025-02-24 17:14:23 +0300

    update login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -10,56 +10,51 @@ class SigninOrSignupScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Spacer(flex: 2),
-              Image.asset("assets/logo.png", height: 200),
-              Text(
-                'add here slogan',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: ColorScheme.of(context).secondary,
-                  fontStyle: FontStyle.italic,
-                ),
+              // App Logo at the top
+              Column(
+                children: [
+                  const SizedBox(height: 100),
+                  Image.asset("assets/logo.png", height: 200),
+                  const SizedBox(height: 30),
+                  Text(
+                    "Welcome to Our App!",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Welcome to the World of Ciphers: Mastering the Art of Concealed Communication!",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey[600],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
               ),
+
               const Spacer(),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  width: 200,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    color: ColorScheme.of(context).onPrimary,
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: const [
-                      BoxShadow(
-                        color: Color(0xFF8A2BE2),
-                        spreadRadius: 3,
-                        blurRadius: 10,
-                        offset: Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      shadowColor: Colors.transparent,
-                    ),
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, '/home');
-                    },
-                    child: const Text(
-                      'Get Started',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
+
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacementNamed(context, '/home');
+                },
+                style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  foregroundColor: Colors.white,
+                  minimumSize: const Size(double.infinity, 48),
+                  shape: const StadiumBorder(),
+                  backgroundColor: Colors.deepPurple,
                 ),
+                child: const Text("Get Started"),
               ),
-              const Spacer(flex: 3),
+              const SizedBox(height: 32), // Space below the button
             ],
           ),
         ),
