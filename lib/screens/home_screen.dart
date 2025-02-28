a241f5b58157336e78f5ea8c5bdb938fd3e02e58
Mustafa Kazı <mkazi581@gmail.com>
2025-02-28 22:20:48 +0300

    update main screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,7 +13,6 @@ class _HomeScreenState extends State<HomeScreen> {
   int _currentIndex = 1;
 
   final List<Widget> _pages = [
-    const Center(child: Text('Temp 01', style: TextStyle(fontSize: 24))),
     const Ciphers(),
     const Center(child: Text('Temp 03', style: TextStyle(fontSize: 24))),
   ];
@@ -24,12 +23,10 @@ class _HomeScreenState extends State<HomeScreen> {
       body: _pages[_currentIndex],
       bottomNavigationBar: CircleNavBar(
         activeIcons: const [
-          Icon(Icons.person),
           Icon(Icons.catching_pokemon),
           Icon(Icons.settings),
         ],
         inactiveIcons: const [
-          Text("Profile"),
           Text("Home"),
           Text("Settings"),
         ],
