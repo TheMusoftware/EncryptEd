c815e5dec579a801e6e637a11482be2448bda5d3
Liubou <132359274+LiubovMtsvch@users.noreply.github.com>
2025-03-16 20:21:53 +0300

    standartization.dart

--- lib/screens/algorithms_screen.dart
+++ lib/screens/algorithms_screen.dart
@@ -1,3 +1,4 @@
+import 'package:encrypt_ed/screens/standartization.dart';
 import 'package:flutter/material.dart';
 import 'double_square.dart';
 
@@ -28,15 +29,22 @@ class SettingsScreen extends StatelessWidget {
                 );
               },
             ),
-            const SizedBox(height: 12),
+            const SizedBox(height: 1),  // уменьшено расстояние между карточками
             _buildSettingCard(
               context,
               imagePath: 'assets/devil_magic_square.png',
-              title: 'Магический квадрат',
+              title: 'Криптографическая стандартизация ',
               description: 'Таблица с равными суммами в строках, столбцах.',
-              onTap: () {},
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Standartization(),
+                  ),
+                );
+              },
             ),
-            const SizedBox(height: 12),
+            const SizedBox(height: 1),  // уменьшено расстояние между карточками
             _buildSettingCard(
               context,
               imagePath: 'assets/logo.png',
@@ -57,33 +65,21 @@ class SettingsScreen extends StatelessWidget {
         required String description,
         required VoidCallback onTap,
       }) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-
-    double cardWidth = screenWidth * 0.9;
-    double cardHeight = screenHeight * 0.25;
-    double imageWidth = screenWidth * 0.36;
-    double imageHeight = cardHeight * 0.84;
-
-    double titleFontSize = screenWidth * 0.045;
-    double descriptionFontSize = screenWidth * 0.04;
-
     return Container(
-      margin: const EdgeInsets.only(bottom: 8.0),
-      height: cardHeight,
-      width: cardWidth,
+      margin: const EdgeInsets.only(bottom: 1),  // уменьшено расстояние между карточками
+      height: 230,  // уменьшена высота карточки
       child: Stack(
         children: [
           Positioned(
-            top: cardHeight * 0.15,
-            left: screenWidth * 0.05,
+            top: 35,
+            left: 10,
             child: Material(
               elevation: 5.0,
               shadowColor: Colors.purple.withOpacity(0.6),
               borderRadius: BorderRadius.circular(7.0),
               child: Container(
-                height: cardHeight - 40,
-                width: cardWidth - 20,
+                height: 140,
+                width: 315,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(7.0),
@@ -93,7 +89,7 @@ class SettingsScreen extends StatelessWidget {
           ),
           Positioned(
             top: 0,
-            left: screenWidth * 0.05,
+            left: 20,
             child: Card(
               elevation: 10.0,
               shadowColor: Colors.purple.withOpacity(0.6),
@@ -101,8 +97,8 @@ class SettingsScreen extends StatelessWidget {
                 borderRadius: BorderRadius.circular(15.0),
               ),
               child: Container(
-                height: imageHeight,
-                width: imageWidth,
+                height: 150,
+                width: 105,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(10.0),
                   image: DecorationImage(
@@ -114,47 +110,49 @@ class SettingsScreen extends StatelessWidget {
             ),
           ),
           Positioned(
-            top: cardHeight * 0.2,
-            left: screenWidth * 0.45,
+            top: 40,
+            left: 145,
             child: SizedBox(
-              height: cardHeight - 40,
-              width: cardWidth - imageWidth - 20,
+              height: 150,
+              width: 180,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     title,
-                    style: TextStyle(
-                      fontSize: titleFontSize,
+                    style: const TextStyle(
+                      fontSize: 18,  // установлено фиксированное значение для шрифта
                       color: Colors.black,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                   const SizedBox(height: 6),
-                  Expanded(
-                    child: SingleChildScrollView(
-                      child: Text(
-                        description,
-                        style: TextStyle(
-                          fontSize: descriptionFontSize,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
+                  Text(
+                    description,
+                    style: const TextStyle(
+                      fontSize: 12,  // установлено фиксированное значение для шрифта
+                      color: Colors.grey,
+                      fontWeight: FontWeight.normal,
                     ),
                   ),
+                  const SizedBox(height: 1),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: TextButton(
+                          onPressed: onTap,
+                          child: const Text(
+                            'Подробнее',
+                            style: TextStyle(fontSize: 13),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 3),
+                    ],
+                  )
                 ],
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 8,
-            right: 8,
-            child: TextButton(
-              onPressed: onTap,
-              child: const Text(
-                'Подробнее',
-                style: TextStyle(fontSize: 14),
               ),
             ),
           ),
